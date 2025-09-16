233a9d55870e107a6f181aa16497f77b8588070a
adisaljusi <adis.aljusi@hotmail.com>
2025-09-16 15:20:41 +0200

    Initialize Spring Boot project

---
+++ senf/src/main/java/com/adisaljusi/senf/SenfApplication.java
@@ -0,0 +1,13 @@
+package com.adisaljusi.senf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenfApplication.class, args);
+	}
+
+}
---
+++ senf/src/test/java/com/adisaljusi/senf/SenfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adisaljusi.senf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
